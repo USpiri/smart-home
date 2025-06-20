54d26a669fbc423dd776185fbdae7c36bbe1992f
USpiri <urielspiridione135@gmail.com>
2025-06-20 10:32:49 -0300

    feat: add theme functionality

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,7 +1,11 @@
+import { useThemeStore } from "@/store/theme.store";
 import { Button } from "./ui/button";
-import { QrCode, Sun } from "lucide-react";
+import { Moon, QrCode, Sun } from "lucide-react";
 
 export const Topbar = () => {
+  const theme = useThemeStore((s) => s.theme);
+  const setTheme = useThemeStore((s) => s.setTheme);
+
   return (
     <div className="bg-background sticky h-14 flex items-center justify-between px-4 top-0 border-b border-border">
       <h1 className="text-lg font-mono">Smart home</h1>
@@ -10,8 +14,17 @@ export const Topbar = () => {
           <QrCode className="size-4" />
           <span className="sr-only">QR Code</span>
         </Button>
-        <Button variant="ghost" size="icon" className="text-muted-foreground">
-          <Sun className="w-4 h-4" />
+        <Button
+          variant="ghost"
+          size="icon"
+          className="text-muted-foreground"
+          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        >
+          {theme === "dark" ? (
+            <Sun className="size-4" />
+          ) : (
+            <Moon className="size-4" />
+          )}
           <span className="sr-only">Toggle theme</span>
         </Button>
       </div>
--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -1,7 +1,17 @@
 import { Outlet } from "react-router";
 import { Topbar } from "../Topbar";
+import { useThemeStore } from "@/store/theme.store";
+import { useEffect } from "react";
 
 export const AppLayout = () => {
+  const theme = useThemeStore((s) => s.theme);
+  const setTheme = useThemeStore((s) => s.setTheme);
+
+  // Set the theme
+  useEffect(() => {
+    setTheme(theme);
+  }, []);
+
   return (
     <div>
       <Topbar />
