4e70b2d944dd3ca7a68d609ab03dd215300d0e9b
USpiri <urielspiridione135@gmail.com>
2025-06-20 23:32:50 -0300

    fix: Rename createRoom function

--- src/actions/room/create-room.ts
+++ src/actions/room/create-room.ts
@@ -2,7 +2,7 @@ import { createActionResponse } from "@/lib/actions/create";
 import { db, RoomSchema } from "@/lib/db";
 import type { RoomInsert } from "@/types";
 
-export const createDevice = async (room: RoomInsert) => {
+export const createRoom = async (room: RoomInsert) => {
   try {
     const result = await db.insert(RoomSchema).values(room).returning();
     return createActionResponse.success(result[0]);
