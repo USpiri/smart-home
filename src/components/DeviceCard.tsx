f2e416e43c90d5d78230ae5e4b27be3fb7af713e
USpiri <urielspiridione135@gmail.com>
2025-06-21 12:53:12 -0300

    feat: Enhance DeviceCard to manage device state with toggle functionality and connection status

--- src/components/DeviceCard.tsx
+++ src/components/DeviceCard.tsx
@@ -1,23 +1,55 @@
 import { Button } from "@/components/ui/button";
 import { Switch } from "@/components/ui/switch";
 import type { Device } from "@/types";
-import { MoreHorizontal, Pin, Share2, ToggleLeft } from "lucide-react";
+import {
+  MoreHorizontal,
+  Pin,
+  Share2,
+  ToggleLeft,
+  ToggleRight,
+} from "lucide-react";
 import { Link } from "react-router";
+import { useLocaleDeviceState, useLocalDeviceMutation } from "@/hooks";
+import { ConnectionStatus } from "./ConnectionStatus";
 
 interface Props {
   device: Device;
 }
 
 export const DeviceCard = ({ device }: Props) => {
+  const {
+    query: { isLoading, error, data: deviceState },
+  } = useLocaleDeviceState(device.ip);
+  const { onMutation, offMutation } = useLocalDeviceMutation(device.ip);
+
   return (
     <article className="border-border rounded-md border px-4 py-3">
       <header className="flex items-center justify-between gap-2">
         <Link to={`/devices/${device.id}`} className="flex items-center gap-2">
-          <ToggleLeft className="size-4" />
+          {deviceState?.status === "on" ? (
+            <ToggleRight className="size-4 text-emerald-500" />
+          ) : (
+            <ToggleLeft className="size-4" />
+          )}
           <h2 className="font-mono text-lg font-bold">{device.name}</h2>
-          <div className="rounded-full bg-emerald-300 p-1" />
+          <ConnectionStatus
+            size="sm"
+            status={
+              isLoading ? "connecting" : error ? "disconnected" : "connected"
+            }
+          />
         </Link>
-        <Switch className="data-[state=checked]:bg-emerald-500" />
+        <Switch
+          className="data-[state=checked]:bg-emerald-500"
+          checked={deviceState?.status === "on"}
+          onCheckedChange={() => {
+            if (deviceState?.status === "on") {
+              offMutation.mutate();
+            } else {
+              onMutation.mutate();
+            }
+          }}
+        />
       </header>
       <div className="mt-2 flex items-center justify-between">
         <p className="text-muted-foreground font-mono text-xs">{device.type}</p>
