f9f0e2c014d84b28a6d5ce1e4859ce518a0ac51c
USpiri <urielspiridione135@gmail.com>
2025-06-21 14:19:39 -0300

    fix: Make Bottombar sticky at the bottom of the viewport

--- src/components/Bottombar.tsx
+++ src/components/Bottombar.tsx
@@ -3,7 +3,7 @@ import { BottombarItem } from "./BottombarItem";
 
 export const Bottombar = () => {
   return (
-    <nav className="bg-background dark:border-border flex h-14 w-full items-center shadow-[0_-2px_4px_rgba(0,0,0,0.1)] md:h-16 dark:border-t">
+    <nav className="bg-background dark:border-border sticky bottom-0 flex h-14 w-full items-center shadow-[0_-2px_4px_rgba(0,0,0,0.1)] md:h-16 dark:border-t">
       <div className="container mx-auto flex w-full items-center justify-around lg:max-w-4xl">
         <BottombarItem to="/" Icon={Home} label="Home" />
         <BottombarItem to="/rooms" Icon={Building2} label="Rooms" />
