a751f58b549a37ebacbd2545679e6ab2c1e7260b
USpiri <urielspiridione135@gmail.com>
2025-06-23 00:53:49 -0300

    feat: integrate Toaster component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { AppProviders } from "./components/providers/AppProviders";
+import { Toaster } from "./components/ui/sonner";
 import { AppRouter } from "./router/AppRouter";
 
 function App() {
   return (
     <AppProviders>
       <AppRouter />
+      <Toaster />
     </AppProviders>
   );
 }
