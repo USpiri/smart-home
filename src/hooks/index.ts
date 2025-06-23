ef7ba884af5e225949f332b0eeac3ac497a41e09
USpiri <urielspiridione135@gmail.com>
2025-06-22 23:02:35 -0300

    feat: export useQrScanner hook in barrel

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
-export * from './useLocalDeviceMutation';
-export * from './useLocaleDeviceState';
+export * from "./useLocalDeviceMutation";
+export * from "./useLocaleDeviceState";
+export * from "./useQrScanner";
